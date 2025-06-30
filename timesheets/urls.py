bf36be1e38f4294bf6d5d2ed58ce1e0c6d17f0c1
demetermarcell <demeter.marcell@gmail.com>
2025-07-01 00:16:30 +0100

    Add project_dashboard view incl. urls and htmls

--- timesheets/urls.py
+++ timesheets/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.project_dashboard, name='project_dashboard'),
+    path('', views.projects_dashboard, name='projects_dashboard'),
+    path('project/<int:pk>/', views.project_view, name='project_view'),
 ]
--- timesheets/views.py
+++ timesheets/views.py
@@ -1,8 +1,24 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
 
 # Create your views here.
 @login_required
-def project_dashboard(request):
-    return render(request, 'timesheets/project_dashboard.html')
+def projects_dashboard(request):
+    user = request.user
+    owned_projects = Project.objects.filter(owner=user)
+    assigned_projects = Project.objects.filter(contributors=user).exclude(owner=user)
+
+    context = {
+        'owned_projects': owned_projects,
+        'assigned_projects': assigned_projects,
+    }
+    return render(request, 'timesheets/projects_dashboard.html', context)
+
+
+
+@login_required
+def project_view(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, 'timesheets/project_view.html', {'project': project})
