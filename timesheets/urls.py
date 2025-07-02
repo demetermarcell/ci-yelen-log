92698e0b5e97b08600d3a42c81c7e3535bf86300
demetermarcell <demeter.marcell@gmail.com>
2025-07-02 03:16:17 +0100

    Add raw project view, timesheet table and view

--- timesheets/urls.py
+++ timesheets/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.projects_dashboard, name='projects_dashboard'),
     path('project/<slug:slug>/', views.project_view, name='project_view'),
+    path('timesheet/<slug:slug>/', views.timesheet_view, name='timesheet_view'),
+
 ]
--- timesheets/views.py
+++ timesheets/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-from .models import Project
+from .models import Project, Timesheet
 
 
 # Create your views here.
@@ -8,7 +8,7 @@ from .models import Project
 def projects_dashboard(request):
     user = request.user
     owned_projects = Project.objects.filter(owner=user)
-    assigned_projects = Project.objects.filter(contributors=user).exclude(owner=user)
+    assigned_projects = Project.objects.filter(contributors=user)
 
     context = {
         'owned_projects': owned_projects,
@@ -21,4 +21,35 @@ def projects_dashboard(request):
 @login_required
 def project_view(request, slug):
     project = get_object_or_404(Project, slug=slug)
-    return render(request, 'timesheets/project_view.html', {'project': project})
+    is_owner = request.user == project.owner
+
+    # Timesheets by user for this project
+    my_timesheets = Timesheet.objects.filter(
+        user=request.user, project=project
+    ).order_by('-start_date')
+
+    # All project timesheets, only visible to owner
+    project_timesheets = Timesheet.objects.filter(
+        project=project,
+        status__in=['submitted', 'approved', 'rejected']
+    ).order_by('-submitted_on') if is_owner else None
+
+    contributors = project.contributors.all() if is_owner else None
+
+    context = {
+        'project': project,
+        'is_owner': is_owner,
+        'contributors': contributors,
+        'my_timesheets': my_timesheets,
+        'project_timesheets': project_timesheets,
+    }
+    return render(request, 'timesheets/project_view.html', context)
+
+
+@login_required
+def timesheet_view(request, slug):
+    timesheet = get_object_or_404(Timesheet, slug=slug)
+
+    return render(request, 'timesheets/timesheet_view.html', {
+        'timesheet': timesheet,
+    })
